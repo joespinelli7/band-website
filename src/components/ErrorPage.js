8ff6c96ac6743fbdaa970cb4e7b878637bbf26ef
github username <josephspinelli@yahoo.com>
2019-11-22 23:46:39 -0600

    error page

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -3,7 +3,7 @@ import './ErrorPage.css';
 
 const ErrorPage = () => {
   return (
-    <div className="col">
+    <div>
       <h3 className="error"><span role="img" aria-label="Error">❌</span>Something went wrong, can't find page you're looking for.<span role="img" aria-label="Error">❌</span></h3>
     </div>
   )
